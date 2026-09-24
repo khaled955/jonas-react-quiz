f72c9b3bf5b5beb1e60e9a7274d983628a2fb12a
khaled955 <khaledmansour104@yahoo.com>
2026-09-24 10:53:01 +0300

    fix json import

--- api/questions.ts
+++ api/questions.ts
@@ -1,9 +1,6 @@
+import data from "../data/questions.json" with { type: "json" };
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import data from "../data/questions.json";
 
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse,
-) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   return res.status(200).json(data.questions);
 }
